75e649333e2b0af2ec12f7eac58a660b02cd8a4e
theWorthyG <wdg2001@wildcats.unh.edu>
2019-03-13 19:52:08 -0400

    Liri Bot v1

---
+++ Keys.js
@@ -0,0 +1,14 @@
+
+// console.log('this loaded');
+
+exports.twitterKeys = {
+  consumer_key: '8MKZqeyIvF2gZTClyxjojXIIU',
+  consumer_secret: 'sXjGZfOLmBCdTUgQhsxxnWTmuQ0U3ZfIorm2zrXPVUnxguSdTV',
+  access_token_key: '1105569529416085504-9skOTn1aPP210j4k4drKylJLVaV8Vo',
+  access_token_secret: 'd4H3zgTPyEjQ5uRwwysfjh48mmBVeJFJT53qScLuSmtKm',
+}
+
+exports.spotifyKeys = {
+	id: 'c7bbd104ccf1460daa05826c28f3dc7c',
+	secret: '907b97e055294d6ea2592591b7f24306',
+}
